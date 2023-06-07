c65fd052330f557f18df0b122bb44857e9f8362a
Adair Soto <91437559+adairsoto@users.noreply.github.com>
2023-06-07 09:51:18 -0300

    Update packages

--- src/components/greet/Greet.test.js
+++ src/components/greet/Greet.test.js
@@ -1,14 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import Greet from './greet';
 
-test('renders greet', () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
+describe('Greet', () => {
+  test('renders greet', () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
+  
+  test('renders greet with a name', () => {
+      render(<Greet name='Paul' />);
+      const textElement = screen.getByText(/hello paul/i);
+      expect(textElement).toBeInTheDocument();
+  });
 });
 
-test('renders greet with a name', () => {
-    render(<Greet name='Paul' />);
-    const textElement = screen.getByText(/hello paul/i);
-    expect(textElement).toBeInTheDocument();
-});
+// describe can be nested
+
+// filename conventions
+// .test.js or .test.tsx
+// .spec.js or .spec.tsx
+// .js or .tsx in __tests__ folders
+
+// alternatives to test method:
+// test -> it
+// test.only -> fit
+// test.skip -> xit
